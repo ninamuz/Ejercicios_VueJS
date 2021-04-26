5bec9699321ec2753f0b06db0a5d53621ca08e9c
ninamuz <gia.muzzio@gmail.com>
2021-04-26 15:07:31 -0400

    agrego ejercicio formulario

---
+++ ejercicio_check/index.js
@@ -0,0 +1,21 @@
+let app = new Vue({
+    el: "#app",
+    data: {
+        nombre: "",
+        peliculasterror: "",
+        peliculascomedia: "",
+        peliculasromance: "",
+        peliculasaccion: "",
+        peliculas: [],
+        tratamiento: "",
+
+    },
+
+    methods: {
+
+        verificar() {
+            
+        }
+
+    }
+})
